a8bed1d85faac7e7bc0d260eef814a6695cd2274
JGCV2007 <jeangcv@hotmail.com>
2023-08-10 20:32:53 -0300

    concluindo Java script ...

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
+const button = document.querySelector('button')
 const inpunt_day = document.querySelector('iday');
-const inpunt_moth = document.querySelector('imonth');
+const inpunt_month = document.querySelector('imonth');
 const inpunt_year = document.querySelector('iyear');
-
+const error_day = document.querySelector('error.day')
+const wrong_day = document.querySelector('wrong.day')
+const error_month = document.querySelector('error.month')
+const wrong_month = document.querySelector('wrong.month')
+const error_year = document.querySelector('error.year')
+const wrong_year = document.querySelector('wrong.year')
 const today = new Date();
 const day_atual = today.getDay();
-const getMonth_atual = today.getMonth();
+const Month_atual = today.getMonth();
 const year_atual = today.getFullYear();
 
-function submit() {
-    if (inpunt_day == ""){
-        error.day("inpunt_day")
+function calyear(){
+    if (inpunt_year == " ") {
+     error_year.style.display = "block"
     }
-    else if (inpunt_day <1 || inpunt_day >31) {
-        wrong(inpunt_day)
+    else if (inpunt_year <0 || inpunt_year > year_atual) {
+    wrong_year.style.display = "block"
     }
 }
+function calmon(){
+    if (inpunt_month == " ") {
+    error_month.style.display = "block"
+    }
+    else if (inpunt_month <1 || inpunt_month >12) {
+     wrong_month.style.display = "block"
+    }
+}
+function calday(){
+    if (inpunt_day == " "){
+     error_day.style.display = "block"
+    }
+    else if (inpunt_day <1 || inpunt_day >31) {
+    wrong_day.style.display = "block"
+}
+}
+function submit() {
+    if(day_atual && Month_atual < inpunt_day && inpunt_month){
+
+    }
+ year_atual - inpunt_year
+   
+}
+
+ // selecionar output e terminar calculos
 
 
 
@@ -25,26 +56,24 @@ function submit() {
 
 
 
+ //button.addEventListener
 
 
 
-
-
-
-
-
-/*function calyear(){
+/*function calyear(){ oninput="calyear()"
     var ano = new Date().getFullYear()
     iano.innerHTML = Number(ano) - Number(iyear.value) 
 }
 
-function calmon(){
+function calmon(){ oninput="calmon()" 
     var mes = new Date().getMonth()
     imes.innerHTML =  Number(imonth.value)-Number(mes) 
 }
 
-function calday(){ 
+function calday(){ oninput="calday()" 
     var dia = new Date().getDay()
     idia.innerHTML = Number(dia) + Number(iday.value)
 }*/
+//error_day("inpunt_day")
+//wrong_day("inpunt_day")
         
