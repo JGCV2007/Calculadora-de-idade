8d1911f15c6de638106079cea6309f3c805e9bda
JGCV2007 <jeangcv@hotmail.com>
2023-09-08 12:29:14 -0300

    Atualizando conclusão

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button')
+const $button = document.querySelector('button')
 const input_day = document.querySelector('iday');
 const input_month = document.querySelector('imonth');
 const input_year = document.querySelector('iyear');
@@ -12,6 +12,18 @@ const today = new Date();
 const day_atual = today.getDay();
 const Month_atual = today.getMonth();
 const year_atual = today.getFullYear();
+
+
+$button.addEventListener('click', function calculo(){
+    var ano = new Date().getFullYear()
+    iano.innerHTML = Number(ano) - Number(iyear.value) 
+    
+    var mes = new Date().getMonth()
+    imes.innerHTML =  Number(imonth.value)-Number(mes) 
+
+    var dia = new Date().getDay()
+    idia.innerHTML = Number(dia) + Number(iday.value)
+})
 
 function calyear(){
     if (input_year === "") {
@@ -39,7 +51,6 @@ function calday(){
 }
 
 
- // selecionar output e terminar calculos
 
 
 
@@ -50,24 +61,5 @@ function calday(){
 
 
 
- //button.addEventListener
 
-
-
-/*function calyear(){ oninput="calyear()"
-    var ano = new Date().getFullYear()
-    iano.innerHTML = Number(ano) - Number(iyear.value) 
-}
-
-function calmon(){ oninput="calmon()" 
-    var mes = new Date().getMonth()
-    imes.innerHTML =  Number(imonth.value)-Number(mes) 
-}
-
-function calday(){ oninput="calday()" 
-    var dia = new Date().getDay()
-    idia.innerHTML = Number(dia) + Number(iday.value)
-}*/
-//error_day("inpunt_day")
-//wrong_day("inpunt_day")
         
