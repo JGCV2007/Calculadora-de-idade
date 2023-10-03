0ccc7342cf8d0296c6f6fedd639e469abbfd4687
JGCV2007 <jeangcv@hotmail.com>
2023-10-03 16:06:39 -0300

    Atualizado

--- main.js
+++ main.js
@@ -8,48 +8,40 @@ const error_month = document.querySelector('error.month')
 const wrong_month = document.querySelector('wrong.month')
 const error_year = document.querySelector('error.year')
 const wrong_year = document.querySelector('wrong.year')
-const today = new Date();
-const day_atual = today.getDay();
-const Month_atual = today.getMonth();
-const year_atual = today.getFullYear();
-
 
 $button.addEventListener('click', function calculo(){
-    var ano = new Date().getFullYear()
-    iano.innerHTML = Number(ano) - Number(iyear.value) 
+   var year = new Date().getFullYear()
+    iano.innerHTML = Number(year) - Number(iyear.value) 
     
     var mes = new Date().getMonth()
-    imes.innerHTML =  Number(imonth.value)-Number(mes) 
+    imes.innerHTML =  Number(mes) - Number(imonth.value) 
 
     var dia = new Date().getDay()
     idia.innerHTML = Number(dia) + Number(iday.value)
 })
 
-function calyear(){
-    if (input_year === "") {
-     error_year.style.display = "block"
-    }
-    else if (input_year <0 || input_year > year_atual) {
-    wrong_year.style.display = "block"
-    }
-}
-function calmon(){
-    if (input_month === "") {
-    error_month.style.display = "block"
-    }
-    else if (input_month <1 || input_month >12) {
-     wrong_month.style.display = "block"
-    }
-}
-function calday(){
-    if (input_day === ""){
-     error_day.style.display = "block"
-    }
-    else if (input_day <1 || input_day >31) {
-    wrong_day.style.display = "block"
-}
-}
+$button.addEventListener('click', function problema(){
+    if (input_year == " ") {
+        error_year.style.display = "block"
+       }
+       else if (input_year <0 || input_year > year) {
+       wrong_year.style.display = "block"
+       }
 
+       if (input_month == " ") {
+        error_month.style.display = "block"
+        }
+        else if (input_month <1 || input_month >12) {
+         wrong_month.style.display = "block"
+        }
+
+        if (input_day == " "){
+            error_day.style.display = "block"
+           }
+           else if (input_day <1 || input_day >31) {
+           wrong_day.style.display = "block"
+       }
+})
 
 
 
